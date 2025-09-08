0d5088cb334af3407e7aa7c09f14ccea4286f3fd
Gabriele Pappalardo <gabriele.pappalardo@jetbrains.com>
2025-09-08 16:15:56 +0200

    Add skeleton for main.c

---
+++ main_unfinished.c
@@ -0,0 +1,13 @@
+// This file is not available, is up to the student to create it.
+// #include "render.h"
+
+#include <stdio.h>
+
+int main(void) {
+    // You're allowed to modify this function as much as you want.
+    // Please, provide a small description of what you have done and how.
+    // And, try to avoid memory leaks :)
+
+    fprintf(stdout, "Ready to go?\n");
+    return EXIT_SUCCESS;
+}
